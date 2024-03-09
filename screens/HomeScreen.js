65d065901782376a5d7c92edeb23ef436bea3a3d
Juwan Hollingsworth <jhollingsworth@domain.local>
2024-03-09 14:54:01 -0500

    Add assets, fix console log & other errors

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,15 @@
-import { StyleSheet, Text, View, SafeAreaView, Alert } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Alert,
+  Pressable,
+  Image,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import * as Location from "expo-location";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const HomeScreen = () => {
   //Check if location is enabled on mobile device
@@ -14,7 +23,7 @@ const HomeScreen = () => {
     checkIfLocationEnabled();
     // fx to get the current location
     getCurrentLocation();
-  }, [item]);
+  }, []);
   // Check if location services are enabled
   const checkIfLocationEnabled = async () => {
     let enabled = await Location.hasServicesEnabledAsync();
@@ -58,18 +67,21 @@ const HomeScreen = () => {
     }
     //get langitude and longitude of user
     const { coords } = await Location.getCurrentPositionAsync();
-    //log coords
-    console(coords);
-    console(response);
+
     // if coordinates are available
     if (coords) {
       // destructure lat & long
       const { latitude, longitude } = coords;
-
+      //get  reverse geocode response
       let response = await Location.reverseGeocodeAsync({
         latitude,
         longitude,
       });
+
+      //log coords
+      console.log(coords);
+      console.log(response);
+
       for (let item of response) {
         let address = `${item.name} ${item.city} ${item.postalCode}`;
         setDisplayCurrentAddress(address);
@@ -78,6 +90,19 @@ const HomeScreen = () => {
   };
   return (
     <SafeAreaView>
+      <View>
+        <MaterialIcons name="location-on" size={30} color="#fd5c63" />
+        <View>
+          <Text style={{ fontSize: 18, fontWeight: "600" }}>Home</Text>
+          <Text>{displayCurrentAddress}</Text>
+        </View>
+        <Pressable>
+          <Image
+            style={{ width: 50, height: 50, borderRadius: 25 }}
+            src={{ uri: "https://picsum.photos/id/69/200/300" }}
+          />
+        </Pressable>
+      </View>
       <Text>HomeScreen</Text>
     </SafeAreaView>
   );
