b6874d40f1f0e85979e303f6b2e1553c94c599eb
Juwan Hollingsworth <jhollingsworth@domain.local>
2024-03-16 13:37:40 -0400

    implement & style dressitem components

--- components/DressItem.js
+++ components/DressItem.js
@@ -1,14 +1,50 @@
 import { Pressable, StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
-const DressItem = (item) => {
+// destructure item
+const DressItem = ({ item }) => {
   return (
     <View>
-      <View>
-        <Image source={{ uri: item.image }} />
-      </View>
-      <View></View>
-      <Pressable></Pressable>
+      {/* //create a container for each item  */}
+      <Pressable
+        style={{
+          backgroundColor: "#f8f8f8",
+          borderRadius: 8,
+          padding: 10,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          margin: 14,
+        }}
+      >
+        {/* //display image  */}
+        <View>
+          <Image
+            style={{ width: 70, height: 70 }}
+            source={{ uri: item.image }}
+          />
+        </View>
+        {/* Display text  */}
+        <View>
+          <Text>{item.name}</Text>
+          <Text>${item.price}</Text>
+        </View>
+        {/* display pressable button */}
+        <Pressable style={{ width: 80 }}>
+          <Text
+            style={{
+              borderColor: "gray",
+              borderWidth: 0.8,
+              marginVertical: 10,
+              color: "#088f8f",
+              textAlign: "center",
+              padding: 5,
+            }}
+          >
+            Add
+          </Text>
+        </Pressable>
+      </Pressable>
     </View>
   );
 };
